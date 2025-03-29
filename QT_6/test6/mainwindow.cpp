100e194dc390b60079e0fa54c4402bc92c67e943
Azonfox <azonfox@gmail.com>
2025-03-29 16:28:06 +0600

    add Button signals in QT6/test6

--- QT_6/test6/mainwindow.cpp
+++ QT_6/test6/mainwindow.cpp
@@ -14,8 +14,23 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_dial_valueChanged(int value)
+// В ui Designer на этой кнопке ПКМ и "Перейти к слоту..."
+// Тогда получаем этот код здесь и в mainwindow.h файле
+// Добавляем сюда наш код по типу VFP.
+// А в самом ui файле графически стрелками делаем простейшие
+// соединения сигналов+слотов, как у нас кнопка для закрытия окна.
+void MainWindow::on_pushButton1_clicked()
 {
-
+    // При нажатии на кнопку pushButton1  "Моя Button"
+    // выводим на терминал текст ...
+    qDebug() << "Кнопкень нажжатая!";
+    // ...и меняем надпись на форме
+    ui->label_1->setText("Кнопкень\nнажжатая!");
 }
 
+// При нажатии на кнопку colorButton  "COLOR"
+// меняем цвет надписи
+void MainWindow::on_colorButton_clicked()
+{
+    ui->label_1->setStyleSheet("color: red; font-weight: bold;");
+}
--- QT_6/test6/mainwindow.h
+++ QT_6/test6/mainwindow.h
@@ -16,7 +16,13 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_dial_valueChanged(int value);
+
+    // В ui Designer на этой кнопке ПКМ и "Перейти к слоту..."
+    // Тогда получаем этот код здесь и в mainwindow.cpp файле
+    // Добавляем наш код по типу VFP в mainwindow.cpp
+    void on_pushButton1_clicked();
+
+    void on_colorButton_clicked();
 
 private:
     Ui::MainWindow *ui;

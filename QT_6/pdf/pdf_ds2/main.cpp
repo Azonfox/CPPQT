7a35b82e3ece013e3cc6710a532bf020633401e4
Azonfox <azonfox@gmail.com>
2025-04-14 15:58:53 +0600

    fix QT6/PDF-DS2 Font Color newPage ViewPDF WordWrap

--- QT_6/pdf/pdf_ds2/main.cpp
+++ QT_6/pdf/pdf_ds2/main.cpp
@@ -1,5 +1,6 @@
 // Построение PDF файла от DS (второй вариант, последний текст)
-// с настраиваемыми полями страницы
+// с настраиваемыми полями страницы, созданием других страниц и
+// показом страниц в программе просмотра PDF самой OS.
 // Для настройки полей имеются умножения на ноль,
 //  иначе поля выставляются дважды!!!
 
@@ -14,11 +15,20 @@
 #include <QPageSize>
 #include <QPageLayout>
 #include <QDebug>
+// for viewer PDF
+#include <QDesktopServices>
+#include <QUrl>
+
+
 
 class PdfGenerator : public QWidget {
 public:
     PdfGenerator(QWidget *parent = nullptr) : QWidget(parent) {
         QPushButton *generateButton = new QPushButton("Сгенерировать PDF", this);
+        generateButton->setMinimumHeight(60);
+        QFont gbfont("Courier", 14);
+        generateButton->setFont(gbfont);
+
         QVBoxLayout *layout = new QVBoxLayout(this);
         layout->addWidget(generateButton);
         setLayout(layout);
@@ -29,18 +39,20 @@ private:
     void generatePdf() {
         // Текстовые переменные
         QString headerText = "Заголовок документа";
-        QString text1 = "Текст 1";
-        QString text2 = "Текст 2";
+        QString text1 = "Текст 1 для проверки переноса длинных строк ниже по уровню";
+        QString text2 = "Текст 2 для проверки переноса длинных строк ниже по 11111  уровню для проверки переноса длинных строк ниже по уровню";
         QString text3 = "Текст 3";
 
-        // Желаемые поля в миллиметрах
+        // Желаемые поля в миллиметрах от края бумаги
         const double leftMarginMM   = 50.0;
         const double topMarginMM    = 10.0;
-        const double rightMarginMM  = 10.0;
+        const double rightMarginMM  = 20.0;
         const double bottomMarginMM = 10.0;
 
+        QString filenamePDF = "/home/azfox/PROJECT/CPPQT/QT_6/pdf/output.pdf";
+
         // Создаем PDF writer
-        QPdfWriter pdfWriter("output.pdf");
+        QPdfWriter pdfWriter(filenamePDF);
         pdfWriter.setResolution(96); // 96 DPI
         // Устанавливаем размер страницы A4 (210x297 мм)
         pdfWriter.setPageSize(QPageSize(QPageSize::A4));
@@ -71,23 +83,33 @@ private:
         qDebug() << "Размер зоны W:" << workWidthMM;
         qDebug() << "Размер зоны H:" << workHeightMM;
 
+        // Создаем полотно для рисования
         QPainter painter(&pdfWriter);
         if (!painter.isActive()) {
             QMessageBox::critical(this, "Ошибка", "Не удалось инициализировать QPainter");
             return;
         }
 
+        // Печать рамки всей страницы
+        painter.setPen(QPen(Qt::blue, 6, Qt::SolidLine)); // синий,Хпиксель,сплошной
+        painter.drawRect(0, 0 , workWidthMM*96.0/25.4, workHeightMM*96.0/25.4); // *96.0/25.4
+
+        //  Выставляем шрифт и фон
         painter.setRenderHint(QPainter::Antialiasing);
-        QFont font("Arial", 12); // 12 пунктов
-        painter.setFont(font);
+        QFont fontTitle("Arial", 24, QFont::Bold, true);// true-наклонный шрифт
+        painter.setFont(fontTitle);
+        //painter.setPen(Qt::red); //drawTextWithBorder содержит свой цвет пера!
+        painter.setBrush(QColor(173,216,230)); // цвет заливки всего далее, в паре с setPen
+        painter.setBackground(Qt::lightGray); // цвет фона шрифта
+        painter.setBackgroundMode(Qt::OpaqueMode); // Не прозрачная заливка
 
         // 1. Рисуем заголовок (точно отступаем на заданные поля)
         // тут была ошибка DS - двойной запрос на размер поля!!!
         // поставлены нули!
         const double headerHeightMM = 15.0;
         QRectF headerRectMM(
-         0.0, //   marginsMM.left(),
-         0.0, //   marginsMM.top(),
+         0.0, // marginsMM.left(), //Уже был left, еще добавляем??? :)
+         0.0, // marginsMM.top(),  //Уже был top,  еще добавляем??? :)
             workWidthMM,
             headerHeightMM
         );
@@ -103,21 +125,27 @@ private:
         const double blocksTopMM = marginsMM.top()*0 + headerHeightMM + 10.0;
         const double blockWidthMM = (workWidthMM - 2*blockSpacingMM) / 3;
 
+        //  Выставляем фонт
+        painter.setRenderHint(QPainter::Antialiasing);
+        QFont font123("Courier", 14); // 12 пунктов
+        painter.setFont(font123);
+
+
         // Блок 1
         QRectF block1RectMM(
             0.0, //marginsMM.left(),
             blocksTopMM,
             blockWidthMM,
-            blockHeightMM
+            blockHeightMM*2 // тк строка длинная дла переноса
         );
-        drawTextWithBorder(painter, mmToPixels(block1RectMM, pdfWriter), text1, Qt::AlignCenter);
+        drawTextWithBorder(painter, mmToPixels(block1RectMM, pdfWriter), text1, Qt::AlignRight);
 
         // Блок 2
         QRectF block2RectMM(
             marginsMM.left() * 0.0 + blockWidthMM + blockSpacingMM,
             blocksTopMM,
             blockWidthMM,
-            blockHeightMM
+            blockHeightMM*3 // тк строка длинная дла переноса
         );
         drawTextWithBorder(painter, mmToPixels(block2RectMM, pdfWriter), text2, Qt::AlignCenter);
 
@@ -130,9 +158,38 @@ private:
         );
         drawTextWithBorder(painter, mmToPixels(block3RectMM, pdfWriter), text3, Qt::AlignCenter);
 
+
+        // печать на другой странице по простому красной рамки
+        // Она будет под заливкой
+        pdfWriter.newPage();
+        painter.setPen(QPen(Qt::green, 15, Qt::SolidLine)); // красный,Хпиксель,сплошной
+        painter.setBrush(QColor(0,0,200)); // цвет заливки
+        painter.setBackgroundMode(Qt::OpaqueMode); // Не прозрачная заливка
+        painter.drawRect(-30, 100 , workWidthMM, 100); // *96.0/25.4
+        // Опять печать рамки и заливки на всей другой странице
+        // Она закрывает красную рамку!
+        painter.setPen(QPen(Qt::blue, 6, Qt::SolidLine)); // синий,Хпиксель,сплошной
+        painter.setBrush(QColor(173,216,230)); // цвет заливки всего далее, в паре с setPen
+        painter.setBackgroundMode(Qt::OpaqueMode); // Не прозрачная заливка
+        painter.drawRect(0, 0 , workWidthMM*96.0/25.4, workHeightMM*96.0/25.4); // *96.0/25.4
+        // На другой странице Рисуем заголовок после фона!
+        painter.setFont(fontTitle);
+        drawTextWithBorder(painter, mmToPixels(headerRectMM, pdfWriter), headerText, Qt::AlignCenter);
+        // печать на другой странице фигуры
+        // Эта рамка уже сверху заливки, тк распечатано позже
+        painter.setPen(QPen(Qt::yellow, 15, Qt::SolidLine));
+        painter.setBrush(QColor(173,0,0)); // цвет заливки
+        painter.setBackgroundMode(Qt::OpaqueMode); // Не прозрачная заливка
+        painter.drawRect(100, 200 , workWidthMM, 100); // *96.0/25.4
+
         painter.end();
         // Выдаем сообщение
-        QMessageBox::information(this, "Готово", "PDF создан с установленными полями в мм: output.pdf");
+        QMessageBox::information(this, "Готово", "PDF создан с установленными \n полями в мм: output.pdf");
+        // Выводим PDF файл просмотрщиком по умолчанию текщей OS
+        QDesktopServices::openUrl(QUrl::fromLocalFile(filenamePDF));
+
+        // Закрыть приложение при отладке сразу же, чтоб не болталось
+        QCoreApplication::quit();
     }
 
 
@@ -155,7 +212,8 @@ private:
                           const QString &text, Qt::Alignment alignment) {
         painter.setPen(Qt::black);
         painter.drawRect(rect);
-        painter.drawText(rect, alignment, text);
+    //    painter.drawText(rect, alignment, text); // было
+        painter.drawText(rect, Qt::TextWordWrap | alignment, text);
     }
 };
 
@@ -167,12 +225,13 @@ int main(int argc, char *argv[]) {
 
     QMainWindow window;
     window.setWindowTitle("PDF Generator с точными полями");
-    window.setFixedSize(400, 200);
+    window.setFixedSize(300, 200);
 
     PdfGenerator *generator = new PdfGenerator(&window);
     window.setCentralWidget(generator);
 
     window.show();
+
     return app.exec();
 }
 
